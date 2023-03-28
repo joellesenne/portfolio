2cd6d596edbc849cb757c67735598c473534cd32
joellesenne <jfkles@me.com>
2023-03-28 20:58:59 +0200

    Refactor files vs src finder

---
+++ src/StickySliderNav.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const StickySliderNav = () => {
+  const navSticky = document.getElementById('js-nav-sticky');
+  const navTabs = document.querySelectorAll('.js-nav-tab');
+  const slider = document.querySelectorAll('.js-slider');
+  const sticky = navSticky.offsetTop;
+
+  window.addEventListener('scroll', () => NavSticky());
+
+  const NavSticky = () => {
+    if (window.scrollY >= sticky) {
+      navSticky.classList.add('nav-sticky');
+    } else {
+      navSticky.classList.remove('nav-sticky');
+    }
+
+    const changeLinkState = () => {
+      let index = slider.length;
+
+      while (index-- && window.scrollY + 50 < slider[index].offsetTop) {
+        //console.log(index)
+      }
+
+      navTabs.forEach((navTab) => navTab.classList.remove('nav-tab-slider'));
+      navTabs[index].classList.add('nav-tab-slider');
+    };
+    changeLinkState();
+  };
+};
+
+export default StickySliderNav;
